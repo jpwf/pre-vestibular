a00b358c70fa67cd5eca9bd48fa326b34c390a0c
jpwf <joaopedroweydt@gmail.com>
2021-03-04 07:26:25 -0300

    New function onchange added

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,9 @@
-enem = document.getElementById("enem")
-
-enem.addEventListener("click", function(){
+document.getElementById("vestibular-list").onchange=function(){
+    if(this.value=="ENEM"){
+        window.location.href="enem.html" 
+    }
+    if(this.value=="UERJ"){
+        window.location.href="uerj.html"
+    }
     
-    
-})
+}
